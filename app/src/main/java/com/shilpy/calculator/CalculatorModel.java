1058bf1cea33177d44f5a81734642a4bc2223fe9
rockingrahul98 <rockingrahul98@gmail.com>
2016-08-11 23:49:08 +0530

    Adding constructor to Calculator Model

--- app/src/main/java/com/shilpy/calculator/CalculatorModel.java
+++ app/src/main/java/com/shilpy/calculator/CalculatorModel.java
@@ -1,7 +1,13 @@
 package com.shilpy.calculator;
 
+import android.content.Context;
+
 /**
  * Created by Shilpy on 8/11/2016.
  */
 public class CalculatorModel {
+    private Context context;
+    public CalculatorModel(Context context) {
+        this.context=context;
+    }
 }
