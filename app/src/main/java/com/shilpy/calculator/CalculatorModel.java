76057ae7282e75c19e0174d9f30c2005ac0a7644
Shilpy94 <s.shilpy94@gmail.com>
2016-08-13 10:06:03 -0700

    model has been made and it is working properly.

--- app/src/main/java/com/shilpy/calculator/CalculatorModel.java
+++ app/src/main/java/com/shilpy/calculator/CalculatorModel.java
@@ -6,32 +6,25 @@ import android.content.Context;
  * Created by Shilpy on 8/11/2016.
  */
 public class CalculatorModel {
-
-
-
-    MainActivity ma=MainActivity.getInstance1();
-
-    public void calculate(String opr, String str1) {
+int total;
+    public int calculate(String opr, String str1,int total) {
         if(opr=="+"){
-            ma.total+=Integer.parseInt(str1);
-            ma.str1=null;
+            this.total=total+Integer.parseInt(str1);
+
         }
         else if(opr=="-"){
-            ma.total-=Integer.parseInt(str1);
-            ma.str1=null;
+            this.total=total-Integer.parseInt(str1);
+
         }
         else if(opr=="*"){
-            ma.total*=Integer.parseInt(str1);
-            ma.str1=null;
+            this.total=total*Integer.parseInt(str1);
+
         }
         else if(opr=="/"){
-            ma.total/=Integer.parseInt(str1);
-            ma.str1=null;
+            this.total=total/Integer.parseInt(str1);
         }
+
+        return this.total;
     }
 
-    private static CalculatorModel cm=new CalculatorModel();
-    public static CalculatorModel getInstance2(){
-        return cm;
-    }
 }
